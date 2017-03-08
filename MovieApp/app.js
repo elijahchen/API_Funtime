234a1a148746e322474e986a39c0d8706c1dc8da
elijahchen <code.elijahchen@gmail.com>
2017-03-08 15:11:57 -0500

    - Added base routes for MovieApp

--- MovieApp/app.js
+++ MovieApp/app.js
@@ -1,1 +1,24 @@
-ECHO is on.
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.get("/results", function (req, res) {
+    request("http://www.omdbapi.com/?s=california", function (error, response, body) {
+        if(!error && response.statusCode == 200){
+            var results = JSON.parse(body);
+            res.send(results["Search"][0]["Title"]);
+        }
+    });
+});
+
+app.get("/", function (req, res) {
+    res.send("HOME");
+});
+
+app.get("*", function (req, res) {
+    res.send("404 PAGE NOT FOUND");
+});
+
+app.listen(3000, process.env.IP, function () {
+    console.log("Process has started!")
+});
